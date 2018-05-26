97d700c01c03746eb6b277b1890f74228b5df2a0
wzs28150 <wz666s@163.com>
2018-05-26 13:35:07 +0800

    delete

--- app/home/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    'template'               => [
-        // 模板文件名分隔符
-        'view_depr'    => '_',
-        'taglib_pre_load'    => 'app\common\taglib\Clt',
-    ]
-];
--- app/user/config.php
+++
@@ -1,4 +0,0 @@
-<?php
-return [
-    'pathinfo_depr'          => '/',
-];
