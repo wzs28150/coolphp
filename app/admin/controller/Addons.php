789e45294b34709c502653875ee8396510ca3179
wzs28150 <wz666s@163.com>
2018-05-27 13:30:35 +0800

    add

---
+++ app/admin/controller/Addons.php
@@ -0,0 +1,14 @@
+<?php
+namespace app\admin\controller;
+use think\Db;
+use think\Request;
+use think\Controller;
+class Addons extends Common
+{
+    public function _initialize(){
+        parent::_initialize();
+    }
+    public function index(){
+      echo 1;
+    }
+}
